5226ce3335350fde22879e65ff822d9c45108506
Kkrisi <kadarkrisi@gmail.com>
2024-04-24 11:05:46 +0200

    feltoltes orai

---
+++ src/main/Jatek.java
@@ -0,0 +1,10 @@
+
+package main;
+
+
+public class Jatek {
+    
+    void kezdes(){
+        System.out.println("A CsigaFutam elindult...");
+    }
+}
---
+++ src/main/Leiras.java
@@ -0,0 +1,8 @@
+/*
+3 csiga verseenyzik, az nyer aki elsonek beér
+, mindegyik kulonbozo szinu, piros zöld kék
+, és a játék elején fogadunk h melyik fog nyerni,
+5 kör,
+van olyan elehtoseg h minden korbe 20% lehetoseggel random egyik csiganak aktivizalodasra kerul egy csiga turbosito, Egy körben egy csiga 0-3 közötti sebességben, egységben tud menni, 0-t is tud menni
+
+*/
