7b10603a8d0de295170ffb88b448ff454b35de41
Kkrisi <kadarkrisi@gmail.com>
2024-04-25 12:12:31 +0200

    Cigiszunet

--- src/main/Jatek.java
+++ src/main/Jatek.java
@@ -12,11 +12,12 @@ public class Jatek {
     public static void main(String[] args) {
         kezdes();
         haladas();
+
         
     }
     
     static void kezdes(){
-        System.out.println("A CsigaFutam elindult...");
+        System.out.println("A CsigaFutam elindult...\n");
     }
 
 
@@ -28,8 +29,8 @@ public class Jatek {
     
 
     
-    static String[] csigaSzinek = {"\u001B[31m","\u001B[34m","\u001B[32m"};
-    // piros, kek, zold
+    static String[] csigaSzinek = {"\u001B[31m","\u001B[34m","\u001B[32m","\\u001b[37m"};
+    // piros, kek, zold, feher
     
     
     public static int fogadas(){
@@ -37,19 +38,58 @@ public class Jatek {
         System.out.println("Melyik csigára fogadsz, 1-2-3 ?");
         int fogadott = sc.nextInt();
         System.out.printf("\nA %d-s számú csigára fogadott\n\n",fogadott);
-        return fogadott;
-        
+        return fogadott;   
     }
+    
+    
+    
     public static int[]haladas(){
         Random rnd = new Random();
-        for (int i = 0; i < csigaNevek.length; i++) {
-            for (int j = 0; j < 5; j++) {
+        String sep = "";
+        for (int i = 0; i < 5; i++) {
+            System.out.printf(sep + "--- %d. kör ---\n",i+1);
+            for (int j = 0; j < csigaNevek.length; j++) {
             int lepes = rnd.nextInt(0, 3);
-                csigaPontok[i] += lepes;
-                System.out.printf("\n%d.kör\n%d.számú csiga pontjai: %d\n",i,i+1,csigaPontok[i]);
+                csigaPontok[j] += lepes;
+                System.out.printf(sep + "%s %s pontjai: %d\n",csigaSzinek[j],csigaNevek[j],csigaPontok[j]);
+            }
+            System.out.println("");
+            sep += "   ";
+        }
+        nyertes();
+        return csigaPontok;
+    }
+    
+    
+    public static void nyertes(){
+        int max = 0;
+        String nyertes = "";
+        for (int i = 0; i < csigaNevek.length; i++) {
+            while (i>0 || i<csigaNevek.length){
+                
+            int elozo = i-1;
+            int elozo_szam = csigaPontok[elozo];
+            
+            if(csigaPontok[i] > elozo_szam){
+                max = csigaPontok[i];
+                nyertes = csigaNevek[i];
+
+            }
             }
         }
-        return csigaPontok;
+        System.out.printf("A nyertes: %d", nyertes);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    // nyertünk e a fogadásunkal vagy nem
+    public static void fogadasEredmeny(){
+        int semmi = 1;
     }
 
 }
--- src/main/Leiras.java
+++ src/main/Leiras.java
@@ -5,4 +5,14 @@
 5 kör,
 van olyan elehtoseg h minden korbe 20% lehetoseggel random egyik csiganak aktivizalodasra kerul egy csiga turbosito, Egy körben egy csiga 0-3 közötti egységben tud menni, 0-t is tud menni
 
+
+
+
+1. kezdés metódus, fogadás, haladás
+
+TODO:
+boost mód, random 20% al
+UML ábra átdolgozása
+
+
 */
